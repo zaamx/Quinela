8435edf7e7d58ff893dfd213139078054f64232a
Alonso Avila <avila@zaa.mx>
2014-06-05 02:32:14 -0500

    Estabilizacion Visual

--- site_themes/default_site/library/js/scripts.js
+++ site_themes/default_site/library/js/scripts.js
@@ -233,53 +233,7 @@ jQuery(document).ready(function($) {
   });
 
 
-  $("#activacionpaso2").validate({
-        rules: {
-          Nombre: {
-            required: true,
-            accept: "[a-zA-Z]+"
-          },
-          screen_name: {
-            required: true,
-            accept: "[a-zA-Z]+"
-          },
-          terminosos: {
-            required: true
-          }
-        },
-        messages: {
-          Nombre: {
-            required: '*Ingresa tu nombre completo',
-            accept: '*Solo letras para el nombre'
-          },
-          screen_name: {
-            required: '*Ingresa tu nombre de usuario',
-            accept: '*Solo letras para el nombre'
-          },
-          terminosos: {
-            required: '*Debes aceptar términos y condiciones y avisos de privacidad'
-          }
-        },
-        highlight: function(element) {
-            var id_attr = "#" + $( element ).attr("id") + "1";
-            $(element).closest('.form-group').addClass('has-error has-feedback');
-            $(id_attr).removeClass('glyphicon-ok').addClass('glyphicon-remove');         
-        },
-        unhighlight: function(element) {
-            var id_attr = "#" + $( element ).attr("id") + "1";
-            $(element).closest('.form-group').removeClass('has-error');
-            $(id_attr).removeClass('glyphicon-remove').addClass('glyphicon-ok');         
-        },
-        errorElement: 'span',
-        errorClass: 'help-block',
-        errorPlacement: function(error, element) {
-            if(element.parent('.input-group').length) {
-                error.insertAfter(element.parent());
-            } else {
-                error.insertAfter(element);
-            }
-        }      
-  });
+  
 
 
 
